a1792842d29d8d7d84c4b253562b887e16abad11
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-06-27 14:24:51 +0200

    implemented the json_generator basic structure

--- src/json_generator/json_generator.py
+++ src/json_generator/json_generator.py
@@ -1,3 +1,11 @@
 from pathlib import Path
-def generate_json(receipt : Path):
+from receipt_to_array.array_generator import generate_array
+from receipt_enhancer.enhancer import enhance_receipt
+from receipt_parser.parser import parse_receipt
+
+
+def generate_json(receipt_path : Path) -> str:
+    receipt_array = generate_array(str(receipt_path))
+    enhanced_receipt = enhance_receipt(receipt_array)
+    parsed_receipt = parse_receipt(enhanced_receipt)
     pass
